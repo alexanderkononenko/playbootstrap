6977b3cbb49c066a59d41ff6b25006f66f5a882a
alexanderkononenko <alexanderkononenko@gmail.com>
2014-08-13 15:37:02 +0300

    gitignore and navbar changes

--- js/main.js
+++ js/main.js
@@ -8,4 +8,21 @@ $(document).ready(function(){
     $('.popover-dismiss').popover({
         trigger: 'focus'
     })
+
+    $('.navbar').on('affixed.bs.affix', function () {
+        console.log('Affixed!');
+        $('.navbar').animate({
+            opacity: 0.7
+        }, 300, function() {
+            // Animation complete.
+        });
+    });
+    $('.navbar').on('affixed-top.bs.affix', function () {
+        console.log('Affixed-top!');
+        $('.navbar').animate({
+            opacity: 1
+        }, 300, function() {
+            // Animation complete.
+        });
+    });
 });

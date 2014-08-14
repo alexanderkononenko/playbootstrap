554e08db1b82ef58ccaacb9a91658202d747ed69
alexanderkononenko <alexanderkononenko@gmail.com>
2014-08-14 12:18:31 +0300

    logo as image + logo animation on scroll

--- js/main.js
+++ js/main.js
@@ -10,19 +10,29 @@ $(document).ready(function(){
     })
 
     $('.navbar').on('affixed.bs.affix', function () {
-        console.log('Affixed!');
-        $('.navbar').animate({
-            opacity: 0.7
+        //console.log('Affixed!');
+        $('a.navbar-brand').animate({
+            'background-position-x': '-140px',
+            'background-position-y': '-18px',
+            'width': '67px',
+            'height': '50px'
         }, 300, function() {
             // Animation complete.
         });
+
+        $('.navbar').animate({opacity: 0.7}, 300);
     });
     $('.navbar').on('affixed-top.bs.affix', function () {
-        console.log('Affixed-top!');
-        $('.navbar').animate({
-            opacity: 1
+        //console.log('Affixed-top!');
+        $('a.navbar-brand').animate({
+            'background-position-x': '0',
+            'background-position-y': '0',
+            'width': '200px',
+            'height': '150px'
         }, 300, function() {
             // Animation complete.
         });
+
+        $('.navbar').animate({opacity: 1}, 300);
     });
 });
